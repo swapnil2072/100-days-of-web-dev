a05ea03744f8676dab29b8cc25f2150117448445
swapnil2072 <swapnilshinde909693@gmail.com>
2023-09-24 19:18:39 +0530

    adding line breaks

--- javascript_basics/day30/app.js
+++ javascript_basics/day30/app.js
@@ -7,3 +7,8 @@ let hobbies = ["Sports", "Cooking", "Reading"];
 alert(hobbies);
 alert(hobbies[0]);
 let job = { title: "Developer", place: "NewYork", salary: 10000 };
+
+let userName = `ma
+x`;
+let userName1 = 'max'+
+'other string';
