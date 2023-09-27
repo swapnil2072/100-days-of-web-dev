ed9dc96b5f733eacdcf8a13918a869a07e918e9c
swapnil2072 <swapnilshinde909693@gmail.com>
2023-09-27 20:26:22 +0530

    deleting element from page

--- advanced_javascript/working_with_dom/app.js
+++ advanced_javascript/working_with_dom/app.js
@@ -1,18 +1,36 @@
-document.body.children[1].children[0].href = "https://google.com";
+// document.body.children[1].children[0].href = 'https://google.com';
 
 // console.dir(document);
 // alert();
 // window.alert();
 
-// create new element
+let anchorElement = document.getElementById('external-link');
+anchorElement.href = 'https://google.com';
 
-let newAnchorElement = document.createElement("a");
-newAnchorElement.href = "https://google.com";
-newAnchorElement.textContent = "google";
-// get accessed to parent element of that should hold the new element
+anchorElement = document.querySelector('p a'); // p a { color: red; }
+anchorElement.href = 'https://academind.com';
 
-let firstParagraph = document.querySelector("p");
+// ADD AN ELEMENT
+// 1. Create the new element
 
-//insert the new element  into parent element content
+let newAnchorElement = document.createElement('a');
+newAnchorElement.href = 'https://google.com';
+newAnchorElement.textContent = 'This leads to Google!';
+
+// 2. Get access to the parent element that should hold the new element
+
+let firstParagraph = document.querySelector('p');
+
+// 3. Insert the new element into the parent element content
 
 firstParagraph.append(newAnchorElement);
+
+// REMOVE ELEMENTS
+// 1. Select the element that should be removed
+
+let firstH1Element = document.querySelector('h1');
+
+// 2. Remove it!
+
+// firstH1Element.remove();
+firstH1Element.parentElement.removeChild(firstH1Element); // for older browsers
