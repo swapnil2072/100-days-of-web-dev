aca22073b73411c2107deaee23e26792b2ec4ab7
swapnil2072 <swapnilshinde909693@gmail.com>
2023-09-27 20:20:44 +0530

    inserting element into page

--- advanced_javascript/working_with_dom/app.js
+++ advanced_javascript/working_with_dom/app.js
@@ -1,5 +1,18 @@
-document.body.children[1].children[0].href = 'https://google.com';
+document.body.children[1].children[0].href = "https://google.com";
 
 // console.dir(document);
 // alert();
 // window.alert();
+
+// create new element
+
+let newAnchorElement = document.createElement("a");
+newAnchorElement.href = "https://google.com";
+newAnchorElement.textContent = "google";
+// get accessed to parent element of that should hold the new element
+
+let firstParagraph = document.querySelector("p");
+
+//insert the new element  into parent element content
+
+firstParagraph.append(newAnchorElement);
