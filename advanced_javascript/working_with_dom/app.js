205ec4336319cc69993edf7c2e8d7a702781ff6f
swapnil2072 <swapnilshinde909693@gmail.com>
2023-09-28 10:37:13 +0530

    use of eventListener

--- advanced_javascript/working_with_dom/app.js
+++ advanced_javascript/working_with_dom/app.js
@@ -1,43 +1,7 @@
-// document.body.children[1].children[0].href = 'https://google.com';
+let paragraphElement = document.querySelector("p");
 
-// console.dir(document);
-// alert();
-// window.alert();
+function changeParagraphText() {
+  paragraphElement.textContent = "clicked";
+}
 
-let anchorElement = document.getElementById("external-link");
-anchorElement.href = "https://google.com";
-
-anchorElement = document.querySelector("p a"); // p a { color: red; }
-anchorElement.href = "https://academind.com";
-
-// ADD AN ELEMENT
-// 1. Create the new element
-
-let newAnchorElement = document.createElement("a");
-newAnchorElement.href = "https://google.com";
-newAnchorElement.textContent = "This leads to Google!";
-
-// 2. Get access to the parent element that should hold the new element
-
-let firstParagraph = document.querySelector("p");
-
-// 3. Insert the new element into the parent element content
-
-firstParagraph.append(newAnchorElement);
-
-// REMOVE ELEMENTS
-// 1. Select the element that should be removed
-
-let firstH1Element = document.querySelector("h1");
-
-// 2. Remove it!
-
-// firstH1Element.remove();
-firstH1Element.parentElement.removeChild(firstH1Element); // for older browsers
-
-// moving elements
-firstParagraph.parentElement.append(firstParagraph);
-
-//innerHTML
-
-firstParagraph.innerHTML = "This is <strong>important</strong>";
+paragraphElement.addEventListener("click", changeParagraphText);
