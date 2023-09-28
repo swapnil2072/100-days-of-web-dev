99d48005c04e0d8090664b57a5c48eaa50ecd478
swapnil2072 <swapnilshinde909693@gmail.com>
2023-09-28 10:53:36 +0530

    getting entered text using event object

--- advanced_javascript/working_with_dom/app.js
+++ advanced_javascript/working_with_dom/app.js
@@ -1,7 +1,21 @@
-let paragraphElement = document.querySelector("p");
+let paragraphElement = document.querySelector('p');
 
-function changeParagraphText() {
-  paragraphElement.textContent = "clicked";
+function changeParagraphText(event) {
+  paragraphElement.textContent = 'Clicked!';
+  console.log('Paragraph clicked!');
+  console.log(event);
 }
 
-paragraphElement.addEventListener("click", changeParagraphText);
+paragraphElement.addEventListener('click', changeParagraphText);
+
+let inputElement = document.querySelector('input');
+
+function retrieveUserInput(event) {
+  // let enteredText = inputElement.value;
+  let enteredText = event.target.value;
+  // let enteredText = event.data; => This is different!
+  console.log(enteredText);
+  // console.log(event);
+}
+
+inputElement.addEventListener('input', retrieveUserInput);
