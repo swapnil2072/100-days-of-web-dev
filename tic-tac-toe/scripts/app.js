7f844c77e485d5d15dd691391be4541583178a5d
swapnil2072 <swapnilshinde909693@gmail.com>
2023-10-05 17:19:04 +0530

    solved overlay problem

--- tic-tac-toe/scripts/app.js
+++ tic-tac-toe/scripts/app.js
@@ -12,14 +12,14 @@ const formElement = document.querySelector("form");
 const errorsOutputElement = document.getElementById("config-errors");
 const gameAreaElement = document.getElementById("active-game");
 // const gameFieldElements = document.querySelectorAll("#game-board li");
-// const gameBoardElement = document.getElementById("game-board");
+const gameBoardElement = document.getElementById("game-board");
 const activePlayerNameElement = document.getElementById('active-player-name');
 
 const editPlayer1BtnElement = document.getElementById("edit-player-1-btn");
 const editPlayer2BtnElement = document.getElementById("edit-player-2-btn");
 const cancelConfigBtnElement = document.getElementById("cancel-config-btn");
 const startNewGameBtnElement = document.getElementById("start-game-btn");
-const gameBoardElement = document.getElementById('game-board');
+
 
 editPlayer1BtnElement.addEventListener("click", openPlayerConfig);
 editPlayer2BtnElement.addEventListener("click", openPlayerConfig);
