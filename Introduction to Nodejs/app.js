6884e53e13a63a8472b79db00ff4a4aef3a2d2e0
swapnil2072 <swapnilshinde909693@gmail.com>
2023-10-10 07:54:41 +0530

    working with request.url

--- Introduction to Nodejs/app.js
+++ Introduction to Nodejs/app.js
@@ -1,8 +1,13 @@
 const http = require("http");
 
 function handleRequester(request, response) {
-  response.statusCode = 200;// .tell about status of request 
-  response.end("<h1> Hello World!</h1>");// it's return the response or data to client  
+  if (request.url === "/currenttime") {
+    response.statusCode = 200; // .tell about status of request
+    response.end("<h1>" + new Date().toISOString + "</h1>"); // new Date create date object which return date
+  } else if (request.url === "/") {
+    response.statusCode = 200; // .tell about status of request
+    response.end("<h1> Hello World!</h1>"); // it's return the response or data to client
+  }
 }
 
 const server = http.createServer(handleRequester);
