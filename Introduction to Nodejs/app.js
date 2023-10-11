53329677191dae438055d0bc0e6f00c662bf9cc3
swapnil2072 <swapnilshinde909693@gmail.com>
2023-10-11 20:15:25 +0530

    creating server with express and working request and response

--- Introduction to Nodejs/app.js
+++ Introduction to Nodejs/app.js
@@ -1,15 +1,12 @@
-const http = require("http");
+const express = require("express");
 
-function handleRequester(request, response) {
-  if (request.url === "/currenttime") {
-    response.statusCode = 200; // .tell about status of request
-    response.end("<h1>" + new Date().toISOString + "</h1>"); // new Date create date object which return date
-  } else if (request.url === "/") {
-    response.statusCode = 200; // .tell about status of request
-    response.end("<h1> Hello World!</h1>"); // it's return the response or data to client
-  }
-}
+const app = express();
 
-const server = http.createServer(handleRequester);
+app.get("/currenttime", function (req, res) {
+  res.send("<h1>" + new Date().toISOString() + "</h1>");
+});
+app.get("/", function (req, res) {
+  res.send("<h1>hello world</h1>");
+});
 
-server.listen(3000);
+app.listen(3000);
