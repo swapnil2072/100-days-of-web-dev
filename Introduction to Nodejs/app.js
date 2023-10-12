5cd016ba41dc349503981455a805bc0fca4d9de8
swapnil2072 <swapnilshinde909693@gmail.com>
2023-10-12 07:31:24 +0530

    parsing userdata using express

--- Introduction to Nodejs/app.js
+++ Introduction to Nodejs/app.js
@@ -2,11 +2,24 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.urlencoded({extended: false}));
+
 app.get("/currenttime", function (req, res) {
   res.send("<h1>" + new Date().toISOString() + "</h1>");
 });
+
+app.get('/', function(req, res) {
+  res.send('<form action="/store-user" method="POST"><label>Your Name</label><input type="text" name="username"><button>Submit</button></form>');
+});
+
 app.get("/", function (req, res) {
   res.send("<h1>hello world</h1>");
 });
+
+app.post('/store-user',function(req,res){
+  const userName = req.body.username;
+  console.log(userName)
+  res.send('<h1>Username stored!</h1>');
+})
 
 app.listen(3000);
