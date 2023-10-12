2c91cbc25bf23ab6c7d73503dbe577d5f8fd7e7d
swapnil2072 <swapnilshinde909693@gmail.com>
2023-10-12 19:23:02 +0530

    storing data to file

--- Introduction to Nodejs/app.js
+++ Introduction to Nodejs/app.js
@@ -1,25 +1,35 @@
+const fs = require("fs");
+const path = require("path");
+
 const express = require("express");
 
 const app = express();
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/currenttime", function (req, res) {
   res.send("<h1>" + new Date().toISOString() + "</h1>");
 });
 
-app.get('/', function(req, res) {
-  res.send('<form action="/store-user" method="POST"><label>Your Name</label><input type="text" name="username"><button>Submit</button></form>');
+app.get("/", function (req, res) {
+  res.send(
+    '<form action="/store-user" method="POST"><label>Your Name</label><input type="text" name="username"><button>Submit</button></form>',
+  );
 });
 
 app.get("/", function (req, res) {
   res.send("<h1>hello world</h1>");
 });
 
-app.post('/store-user',function(req,res){
+app.post("/store-user", function (req, res) {
   const userName = req.body.username;
-  console.log(userName)
-  res.send('<h1>Username stored!</h1>');
-})
+  const filePath = path.join(__dirname, "data", "users.json");
+  const fileData = fs.readFileSync(filePath);
+  const existingUsers = JSON.parse(fileData);
+
+  existingUsers.push(userName);
+  fs.writeFileSync(filePath, JSON.stringify(existingUsers));
+  res.send("<h1>Username stored!</h1>");
+});
 
 app.listen(3000);
