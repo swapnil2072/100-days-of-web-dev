8970769d295adbe7711f93ba1c826eaf5b102e30
swapnil2072 <swapnilshinde909693@gmail.com>
2023-10-15 07:22:30 +0530

    adding ejs template engine

--- dynamic HTML/app.js
+++ dynamic HTML/app.js
@@ -1,28 +1,41 @@
+const fs = require("fs");
+
 const path = require("path");
 
 const express = require("express");
 
 const app = express();
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 app.use(express.static("public"));
+app.use(express.urlencoded({ exteneded: false }));
 
 app.get("/", function (req, res) {
-  const htmlFilePath = path.join(__dirname, "views", "index.html");
-  res.sendFile(htmlFilePath);
+  res.render("index");
 });
 
 app.get("/restaurants", function (req, res) {
-  const htmlFilePath = path.join(__dirname, "views", "restaurants.html");
-  res.sendFile(htmlFilePath);
+  res.render("restaurants");
 });
 app.get("/confirm", function (req, res) {
-  const htmlFilePath = path.join(__dirname, "views", "confirm.html");
-  res.sendFile(htmlFilePath);
+  res.render("confirm");
 });
+
 app.get("/recommend", function (req, res) {
-  const htmlFilePath = path.join(__dirname, "views", "recommend.html");
-  res.sendFile(htmlFilePath);
+  res.render("recommend");
 });
+app.post("/recommend", function (req, res) {
+  const restaurant = req.body;
+  const filePath = path.join(__dirname, "data", "restaurants.json");
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+  storedRestaurants.push(restaurant);
+  fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+  res.redirect("/confirm");
+});
+
 app.get("/about", function (req, res) {
   const htmlFilePath = path.join(__dirname, "views", "about.html");
   res.sendFile(htmlFilePath);
