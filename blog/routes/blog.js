68d964b29943396f98f77f1e367f901fa1c2b331
swapnil2072 <swapnilshinde909693@gmail.com>
2023-10-30 20:22:57 +0530

    creating posts and new-post route

---
+++ blog/routes/blog.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", function (req, res) {
+  res.redirect("/posts");
+});
+router.get("/posts", function (req, res) {
+  res.render("posts-list");
+});
+router.get("/new-post", function (req, res) {
+  res.render("create-post");
+});
+
+module.exports = router;
