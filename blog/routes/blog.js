c8451aa1f3442cffaa24803f4231160b4da82a15
swapnil2072 <swapnilshinde909693@gmail.com>
2023-11-01 21:36:51 +0530

    Inserting Data With Placeholders (Injecting Dynamic Data)

--- blog/routes/blog.js
+++ blog/routes/blog.js
@@ -11,10 +11,22 @@ router.get("/posts", function (req, res) {
   res.render("posts-list");
 });
 router.get("/new-post", async function (req, res) {
-  const [authors]  = await db.query("SELECT * FROM authors");
-  res.render("create-post",{authors:authors});
+  const [authors] = await db.query("SELECT * FROM authors");
+  res.render("create-post", { authors: authors });
 });
 
-
+router.post("/posts", async function (req, res) {
+  const data = [
+    req.body.title,
+    req.body.summary,
+    req.body.content,
+    req.body.author,
+  ];
+  await db.query(
+    "INSERT INTO posts (title, summary, body, author_id) VALUES(?)",
+    [data],
+  );
+  res.redirect("/posts");
+});
 
 module.exports = router;
