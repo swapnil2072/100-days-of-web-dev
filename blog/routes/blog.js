6b5bba70efc84e2b19493e44af6383892c41b64d
swapnil2072 <swapnilshinde909693@gmail.com>
2023-11-04 07:00:48 +0530

    updating posts

--- blog/routes/blog.js
+++ blog/routes/blog.js
@@ -73,8 +73,20 @@ router.get("/posts/:id/edit", async function (req, res) {
     return res.status(404).render("404");
   }
 
-  res.render("update-post",{post:posts[0]});
+  res.render("update-post", { post: posts[0] });
 });
 
+router.post("/posts/:id/edit", async function (req, res) {
+  const query = `
+  UPDATE posts SET title = ?, summary = ?,body = ?
+  WHERE id = ?`;
+  await db.query(query, [
+    req.body.title,
+    req.body.summary,
+    req.body.content,
+    req.params.id,
+  ]);
+  res.redirect("/posts");
+});
 
 module.exports = router;
