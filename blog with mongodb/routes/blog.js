8bf9c28c31dfd42ddb5896df445b991ec93252c5
swapnil2072 <swapnilshinde909693@gmail.com>
2023-11-09 21:31:32 +0530

    fetching and displaying documents

--- blog with mongodb/routes/blog.js
+++ blog with mongodb/routes/blog.js
@@ -1,29 +1,36 @@
-const express = require("express");
-const mongodb = require("mongodb");
+const express = require('express');
+const mongodb = require('mongodb');
 
-const db = require("../data/database");
+const db = require('../data/database');
 
-const ObjectId = mongodb.ObjectId();
+const ObjectId = mongodb.ObjectId;
 
 const router = express.Router();
 
-router.get("/", function (req, res) {
-  res.redirect("/posts");
+router.get('/', function (req, res) {
+  res.redirect('/posts');
 });
 
-router.get("/posts", function (req, res) {
-  res.render("posts-list");
+router.get('/posts', async function (req, res) {
+  const posts = await db
+    .getDb()
+    .collection('posts')
+    .find({})
+    .project({ title: 1, summary: 1, 'author.name': 1 })
+    .toArray();
+  res.render('posts-list', { posts: posts });
 });
 
-router.get("/new-post", async function (req, res) {
-  const authors = await db.getDb().collection("authors").find().toArray();
-  res.render("create-post", { authors: authors });
+router.get('/new-post', async function (req, res) {
+  const authors = await db.getDb().collection('authors').find().toArray();
+  res.render('create-post', { authors: authors });
 });
 
-router.post("/posts", async function (req, res) {
+router.post('/posts', async function (req, res) {
   const authorId = new ObjectId(req.body.author);
-  const author = await db.getDb
-    .collection("authors")
+  const author = await db
+    .getDb()
+    .collection('authors')
     .findOne({ _id: authorId });
 
   const newPost = {
@@ -37,8 +44,10 @@ router.post("/posts", async function (req, res) {
       email: author.email,
     },
   };
-  const result = await db.getDb().collection("posts").insertOne(newPost);
-  res.redirect("/posts");
+
+  const result = await db.getDb().collection('posts').insertOne(newPost);
+  console.log(result);
+  res.redirect('/posts');
 });
 
 module.exports = router;
