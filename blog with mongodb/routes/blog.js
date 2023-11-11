226828622cec4c61877f5806d528c60f221858f7
swapnil2072 <swapnilshinde909693@gmail.com>
2023-11-11 19:11:30 +0530

    expressJS and handling asynchronous code error

--- blog with mongodb/routes/blog.js
+++ blog with mongodb/routes/blog.js
@@ -50,8 +50,13 @@ router.post("/posts", async function (req, res) {
   res.redirect("/posts");
 });
 
-router.get("/posts/:id", async function (req, res) {
-  const postId = req.params.id;
+router.get("/posts/:id", async function (req, res, next) {
+  let postId = req.params.id;
+  try {
+    postId = new ObjectId(postId);
+  } catch (error) {
+    return next(error);
+  }
   const post = await db
     .getDb()
     .collection("posts")
