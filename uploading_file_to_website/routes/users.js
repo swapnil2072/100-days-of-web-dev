7ee7d72a96ae12d51ef2d901edd0049b668d0a52
swapnil2072 <swapnilshinde909693@gmail.com>
2023-11-12 19:31:24 +0530

    serving uploaded file to website visitors

--- uploading_file_to_website/routes/users.js
+++ uploading_file_to_website/routes/users.js
@@ -15,21 +15,22 @@ const storageConfig = multer.diskStorage({
 const upload = multer({ storage: storageConfig });
 const router = express.Router();
 
-router.get("/", function (req, res) {
-  res.render("profiles");
+router.get("/", async function (req, res) {
+  const users = await db.getDb().collection("users").find().toArray();
+  res.render("profiles", { users: users });
 });
 
 router.get("/new-user", function (req, res) {
   res.render("new-user");
 });
 
-router.post("/profiles", upload.single("image"),async function (req, res) {
+router.post("/profiles", upload.single("image"), async function (req, res) {
   const uploadedImageFile = req.file;
   const userData = req.body;
-  await db.getDb().collection('users').insertOne({
-    name:userData.username,
-    imagePath:uploadedImageFile.path
-  })
+  await db.getDb().collection("users").insertOne({
+    name: userData.username,
+    imagePath: uploadedImageFile.path,
+  });
   res.redirect("/");
 });
 
