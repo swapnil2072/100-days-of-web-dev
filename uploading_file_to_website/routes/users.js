e5ecde8cdf9ed988f49150c67fab8a1b368c2697
swapnil2072 <swapnilshinde909693@gmail.com>
2023-11-12 19:11:59 +0530

    configuring multer in-depth

---
+++ uploading_file_to_website/routes/users.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const multer = require("multer");
+
+const storageConfig = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "images");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storageConfig });
+const router = express.Router();
+
+router.get("/", function (req, res) {
+  res.render("profiles");
+});
+
+router.get("/new-user", function (req, res) {
+  res.render("new-user");
+});
+
+router.post("/profiles", upload.single("image"), function (req, res) {
+  const uploadedImageData = req.file;
+  const userData = req.body;
+  res.redirect("/");
+});
+
+module.exports = router;
