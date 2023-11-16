be16c5034e7a1ec680741c691168d2aa5af8af8d
swapnil2072 <swapnilshinde909693@gmail.com>
2023-11-16 21:32:50 +0530

    sending a post request

--- ajax and asynchoronous js/public/scripts/comments.js
+++ ajax and asynchoronous js/public/scripts/comments.js
@@ -1,5 +1,8 @@
 const loadCommentsBtnElement = document.getElementById('load-comments-btn');
 const commentsSectionElement = document.getElementById('comments');
+const commentsFormElement = document.querySelector('#comments-form form');
+const commentTitleElement = document.getElementById('title');
+const commentTextElement = document.getElementById('text');
 
 function createCommentsList(comments) {
   const commentListElement = document.createElement('ol');
@@ -28,4 +31,23 @@ async function fetchCommentsForPost() {
   commentsSectionElement.appendChild(commentsListElement);
 }
 
+function saveComment(event) {
+  event.preventDefault();
+  const postId = commentsFormElement.dataset.postid;
+
+  const enteredTitle = commentTitleElement.value;
+  const enteredText = commentTextElement.value;
+
+  const comment = { title: enteredTitle, text: enteredText };
+
+  fetch(`/posts/${postId}/comments`, {
+    method: 'POST',
+    body: JSON.stringify(comment),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+}
+
 loadCommentsBtnElement.addEventListener('click', fetchCommentsForPost);
+commentsFormElement.addEventListener('submit', saveComment);
