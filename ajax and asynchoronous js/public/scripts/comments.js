f5fa91ff2bf42c7136af80efb02e068c33d828fa
swapnil2072 <swapnilshinde909693@gmail.com>
2023-11-17 14:59:16 +0530

    finished with ajax

--- ajax and asynchoronous js/public/scripts/comments.js
+++ ajax and asynchoronous js/public/scripts/comments.js
@@ -6,7 +6,7 @@ const commentTextElement = document.getElementById('text');
 
 function createCommentsList(comments) {
   const commentListElement = document.createElement('ol');
-  
+
   for (const comment of comments) {
     const commentElement = document.createElement('li');
     commentElement.innerHTML = `
@@ -23,15 +23,29 @@ function createCommentsList(comments) {
 
 async function fetchCommentsForPost() {
   const postId = loadCommentsBtnElement.dataset.postid;
-  const response = await fetch(`/posts/${postId}/comments`);
-  const responseData = await response.json();
-  
-  const commentsListElement = createCommentsList(responseData);
-  commentsSectionElement.innerHTML = '';
-  commentsSectionElement.appendChild(commentsListElement);
+  try {
+    const response = await fetch(`/posts/${postId}/comments`);
+
+    if (!response.ok) {
+      alert('Fetching comments failed!');
+      return;
+    }
+    const responseData = await response.json();
+
+    if (responseData && responseData.length > 0) {
+      const commentsListElement = createCommentsList(responseData);
+      commentsSectionElement.innerHTML = '';
+      commentsSectionElement.appendChild(commentsListElement);
+    } else {
+      commentsSectionElement.firstElementChild.textContent =
+        'We could not find any comments. Maybe add one?';
+    }
+  } catch (error) {
+    alert('Getting comments failed!');
+  }
 }
 
-function saveComment(event) {
+async function saveComment(event) {
   event.preventDefault();
   const postId = commentsFormElement.dataset.postid;
 
@@ -40,13 +54,23 @@ function saveComment(event) {
 
   const comment = { title: enteredTitle, text: enteredText };
 
-  fetch(`/posts/${postId}/comments`, {
-    method: 'POST',
-    body: JSON.stringify(comment),
-    headers: {
-      'Content-Type': 'application/json'
+  try {
+    const response = await fetch(`/posts/${postId}/comments`, {
+      method: 'POST',
+      body: JSON.stringify(comment),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      fetchCommentsForPost();
+    } else {
+      alert('Could not send comment!');
     }
-  });
+  } catch (error) {
+    alert('Could not send request - maybe try again later!');
+  }
 }
 
 loadCommentsBtnElement.addEventListener('click', fetchCommentsForPost);
