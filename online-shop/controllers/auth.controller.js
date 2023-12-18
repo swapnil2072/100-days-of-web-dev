b891a3c95e18188a988bd5e9df24e9025737a4c4
swapnil2072 <swapnilshinde909693@gmail.com>
2023-12-18 20:27:47 +0530

    flashing Errors and input data onto sessions

--- online-shop/controllers/auth.controller.js
+++ online-shop/controllers/auth.controller.js
@@ -1,12 +1,21 @@
 const User = require("../models/user.model");
 const authUtil = require("../util/authentication");
 const validation = require("../util/validation");
+const sessionFlash = require("../util/session-flash");
 
 function getSignup(req, res) {
   res.render("customer/auth/signup");
 }
 
 async function signup(req, res, next) {
+  const enteredData = {
+    email: req.body.email,
+    password: req.body.password,
+    fullname: req.body.fullname,
+    street: req.body.street,
+    postal: req.body.postal,
+    city: req.body.city,
+  };
   if (
     !validation.userDetailAreValid(
       req.body.email,
@@ -18,6 +27,17 @@ async function signup(req, res, next) {
     ) ||
     !validation.emailIsConfirmed(req.body.email, req.body["confirm-email"])
   ) {
+    sessionFlash.flashDataToSession(
+      req,
+      {
+        errorMessage:
+          "Please check your input.Password must be at least 6 characters long",
+        ...enteredData,
+      },
+      function () {
+        res.redirect("/signup");
+      },
+    );
     res.redirect("/signup");
     return;
   }
@@ -34,6 +54,16 @@ async function signup(req, res, next) {
     const existsAlready = await user.existsAlready();
 
     if (existsAlready) {
+      sessionFlash.flashDataToSession(
+        req,
+        {
+          errorMessage: "User exists already! Try logging in instead",
+          ...enteredData,
+        },
+        function () {
+          res.redirect("/signup");
+        },
+      );
       res.redirect("/signup");
       return;
     }
@@ -60,7 +90,17 @@ async function login(req, res, next) {
     return;
   }
 
+  const sessionErrorData = {
+    errorMessage:
+      "Invalid credentials - please double-check your email and password!",
+    email: user.email,
+    password: user.password,
+  };
+
   if (!existingUser) {
+    sessionFlash.flashDataToSession(req, sessionErrorData, function () {
+      res.redirect("/login");
+    });
     res.redirect("/login");
     return;
   }
@@ -70,6 +110,9 @@ async function login(req, res, next) {
   );
 
   if (!passwordIsCorrect) {
+    sessionFlash.flashDataToSession(req, sessionErrorData, function () {
+      res.redirect("/login");
+    });
     res.redirect("/login");
     return;
   }
