ef079c893dd212ff8469c8d271d2b75b2fb8b3a3
swapnil2072 <swapnilshinde909693@gmail.com>
2023-12-18 16:37:24 +0530

    handling erros in async code

--- online-shop/controllers/auth.controller.js
+++ online-shop/controllers/auth.controller.js
@@ -5,7 +5,7 @@ function getSignup(req, res) {
   res.render("customer/auth/signup");
 }
 
-async function signup(req, res) {
+async function signup(req, res, next) {
   const user = new User(
     req.body.email,
     req.body.password,
@@ -14,9 +14,12 @@ async function signup(req, res) {
     req.body.postal,
     req.body.city,
   );
-
-  await user.signup();
-
+  try {
+    await user.signup();
+  } catch (error) {
+    next(error);
+    return;
+  }
   res.redirect("/login");
 }
 
@@ -24,9 +27,15 @@ function getLogin(req, res) {
   res.render("customer/auth/login");
 }
 
-async function login(req, res) {
+async function login(req, res, next) {
   const user = new User(req.body.email, req.body.password);
-  const existingUser = await user.getUserWithSameEmail();
+  let existingUser;
+  try {
+    existingUser = await user.getUserWithSameEmail();
+  } catch (error) {
+    next(error);
+    return;
+  }
 
   if (!existingUser) {
     res.redirect("/login");
