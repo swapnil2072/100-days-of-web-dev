5d775bc63e4c646ad02ca2df9fb3a9af19bd10ca
swapnil2072 <swapnilshinde909693@gmail.com>
2023-12-19 17:36:53 +0530

    adding products details

--- online-shop/controllers/admin.controller.js
+++ online-shop/controllers/admin.controller.js
@@ -1,4 +1,5 @@
 const Product = require("../models/product.model");
+const { get } = require("../routes/admin.routes");
 
 async function getProducts(req, res, next) {
   try {
@@ -27,8 +28,21 @@ async function createNewProduct(req, res, next) {
   res.redirect("/admin/products");
 }
 
+async function getUpdateProduct(req, res, next) {
+  try {
+    const product = await Product.findById(req.params.id);
+    res.render("admin/products/update-product", { product: product });
+  } catch (error) {
+    next(error);
+  }
+}
+
+function UpdateProduct() {}
+
 module.exports = {
   getProducts: getProducts,
   getNewProduct: getNewProduct,
   createNewProduct: createNewProduct,
+  getUpdateProduct: getUpdateProduct,
+  UpdateProduct: UpdateProduct,
 };
