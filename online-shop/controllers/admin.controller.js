5ea8d23f28e4b570435f1459cd45b5a9951a0c8d
swapnil2072 <swapnilshinde909693@gmail.com>
2023-12-19 16:02:36 +0530

    storing products in database

--- online-shop/controllers/admin.controller.js
+++ online-shop/controllers/admin.controller.js
@@ -1,17 +1,27 @@
-function getProducts(req,res) {
-  res.render('admin/products/all-products')
+const Product = require("../models/product.model");
+
+function getProducts(req, res) {
+  res.render("admin/products/all-products");
 }
 
-function getNewProduct(req,res) {
-  res.render('admin/products/new-product')
+function getNewProduct(req, res) {
+  res.render("admin/products/new-product");
 }
 
-function createNewProduct(req,res) {
-  
+async function createNewProduct(req, res, next) {
+  const product = new Product({ ...req.body, image: req.file.filename });
+
+  try {
+   await product.save();
+  } catch (error) {
+    next(error);
+    return;
+  }
+  res.redirect("/admin/products");
 }
 
 module.exports = {
   getProducts: getProducts,
   getNewProduct: getNewProduct,
-  createNewProduct:createNewProduct
+  createNewProduct: createNewProduct,
 };
