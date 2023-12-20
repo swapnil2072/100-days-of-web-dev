44714dbb42d75b941eacda798393df1d7ead5966
swapnil2072 <swapnilshinde909693@gmail.com>
2023-12-20 13:38:37 +0530

    outputting product details

--- online-shop/controllers/products.controller.js
+++ online-shop/controllers/products.controller.js
@@ -8,6 +8,17 @@ async function getAllProducts(req, res, next) {
     next(error);
   }
 }
+
+async function getProductDetails(req, res, next) {
+  try {
+    const product = await Product.findById(req.params.id);
+    res.render("customer/products/product-details", { product: product });
+  } catch (error) {
+    next(error);
+  }
+}
+
 module.exports = {
   getAllProducts: getAllProducts,
+  getProductDetails: getProductDetails,
 };
