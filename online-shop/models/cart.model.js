8c87d88a9d869f3fd1a6ab59559b45000884edce
swapnil2072 <swapnilshinde909693@gmail.com>
2023-12-20 16:29:39 +0530

    shopping cart logic

--- online-shop/models/cart.model.js
+++ online-shop/models/cart.model.js
@@ -1,6 +1,8 @@
 class Cart {
-  constructor(items = []) {
+  constructor(items = [], totalQuantity = 0, totalPrice = 0) {
     this.items = items;
+    this.totalQuantity = totalQuantity;
+    this.totalPrice = totalPrice;
   }
 
   addItem(product) {
@@ -15,9 +17,16 @@ class Cart {
         cartItem.quantity = cartItem.quantity + 1;
         cartItem.totalPrice = cartItem.totalPrice + product.price;
         this.items[i] = cartItem;
+
+        this.totalQuantity++;
+        this.totalPrice += product.price;
         return;
       }
     }
-    this.items.push(product);
+    this.items.push(cartItem);
+    this.totalQuantity++;
+    this.totalPrice += product.price;
   }
 }
+
+module.exports = Cart;
