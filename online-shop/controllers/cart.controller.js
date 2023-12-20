b9bb83927132fa4d26351fb07207205e6faa8861
swapnil2072 <swapnilshinde909693@gmail.com>
2023-12-20 22:40:29 +0530

    updating dom after cart items updates

--- online-shop/controllers/cart.controller.js
+++ online-shop/controllers/cart.controller.js
@@ -27,18 +27,21 @@ async function addCartItem(req, res, next) {
 function updateCartItem(req, res) {
   const cart = res.locals.cart;
 
- const updatedItemData =  cart.updateItem(req.body.product, req.body.quantity);
+  const updatedItemData = cart.updateItem(
+    req.body.productId,
+    req.body.quantity,
+  );
 
   req.session.cart = cart;
 
   res.json({
-    message:'Item updated!',
-    updatedCartData:{
-        newTotalQuantity:cart.totalQuantity,
-        newTotalPrice:cart.totalPrice,
-        updatedItemPrice: updatedItemData.updatedItemPrice
-    }
-  })
+    message: "Item updated!",
+    updatedCartData: {
+      newTotalQuantity: cart.totalQuantity,
+      newTotalPrice: cart.totalPrice,
+      updatedItemPrice: updatedItemData.updatedItemPrice,
+    },
+  });
 }
 
 module.exports = {
