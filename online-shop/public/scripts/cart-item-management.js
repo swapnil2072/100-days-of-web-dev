4ede708d98d327c34edc0d43780b8d74daaaaad0
swapnil2072 <swapnilshinde909693@gmail.com>
2023-12-21 12:49:19 +0530

    not found middleware and mobile badge fix

--- online-shop/public/scripts/cart-item-management.js
+++ online-shop/public/scripts/cart-item-management.js
@@ -1,8 +1,8 @@
 const cartItemUpdateFormElements = document.querySelectorAll(
-  '.cart-item-management'
+  ".cart-item-management",
 );
-const cartTotalPriceElement = document.getElementById('cart-total-price');
-const cartBadge = document.querySelector('.nav-items .badge');
+const cartTotalPriceElement = document.getElementById("cart-total-price");
+const cartBadgeElements = document.querySelectorAll(".nav-items .badge");
 
 async function updateCartItem(event) {
   event.preventDefault();
@@ -15,24 +15,24 @@ async function updateCartItem(event) {
 
   let response;
   try {
-    response = await fetch('/cart/items', {
-      method: 'PATCH',
+    response = await fetch("/cart/items", {
+      method: "PATCH",
       body: JSON.stringify({
         productId: productId,
         quantity: quantity,
         _csrf: csrfToken,
       }),
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     });
   } catch (error) {
-    alert('Something went wrong!');
+    alert("Something went wrong!");
     return;
   }
 
   if (!response.ok) {
-    alert('Something went wrong!');
+    alert("Something went wrong!");
     return;
   }
 
@@ -42,7 +42,7 @@ async function updateCartItem(event) {
     form.parentElement.parentElement.remove();
   } else {
     const cartItemTotalPriceElement =
-      form.parentElement.querySelector('.cart-item-price');
+      form.parentElement.querySelector(".cart-item-price");
     cartItemTotalPriceElement.textContent =
       responseData.updatedCartData.updatedItemPrice.toFixed(2);
   }
@@ -50,9 +50,11 @@ async function updateCartItem(event) {
   cartTotalPriceElement.textContent =
     responseData.updatedCartData.newTotalPrice.toFixed(2);
 
-  cartBadge.textContent = responseData.updatedCartData.newTotalQuantity;
+  for (const cartBadgeElement of cartBadgeElements)
+    cartBadgeElement.textContent =
+      responseData.updatedCartData.newTotalQuantity;
 }
 
 for (const formElement of cartItemUpdateFormElements) {
-  formElement.addEventListener('submit', updateCartItem);
+  formElement.addEventListener("submit", updateCartItem);
 }
